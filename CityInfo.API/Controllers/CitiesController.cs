3a805023c0d5fce28f5a617c63e9b964a7fb74d8
Kumar Krishnamurthy <kumar.sendhi@outlook.com>
2022-10-14 22:25:36 -0500

    datastore setup

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -11,12 +11,15 @@ namespace CityInfo.API.Controllers
         public JsonResult GetCities()
         {
             return new JsonResult(
-                new List<object>
-                {
-                    new {id=1,Name="New York City"},
-                    new {id=2, Name="Antwerp"}
-                }
+               CitiesDataStore.Current.Cities
                 );
+        }
+
+        [HttpGet("{id}")]
+        public JsonResult GetCity(int id)
+        {
+            return new JsonResult(
+                CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id));
         }
     }
 }
